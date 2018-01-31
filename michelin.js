ae67b7a22d899eb92c87fbf3ecc43babe002e2ae
Thomasguillaume <thomaseq.guillaume@gmail.com>
2018-01-31 11:12:23 +0100

    feat: begin the creation of the michelin module

---
+++ michelin.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+
+app.get('/scrape', function(req, res){
+    // The URL we will scrape from.
+
+    url = 'https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin';
+
+    // The structure of our request call
+    // The first parameter is our URL
+    // The callback function takes 3 parameters, an error, response status code and the html
+
+    request(url, function(error, response, html){
+
+        // First we'll check to make sure no errors occurred when making the request
+
+        if(!error){
+            // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+
+            var $ = cheerio.load(html);
+
+            // Finally, we'll define the variables we're going to capture
+
+            var name, cooking, price, review;
+            var json = { name : "", cooking : "", price : "", review : ""};
+        }
+    })
+})
+
+app.listen('8081')
+exports = module.exports = app;
